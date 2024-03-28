d583994e77e6057a7df0d5002fd2c4c4c75ac360
akl5sz <akl5sz@virginia.edu>
2024-03-28 19:07:35 -0400

    made randomValues a var for the controller

--- Hw5/CategoryGuessingController.php
+++ Hw5/CategoryGuessingController.php
@@ -2,14 +2,13 @@
 class CategoryGuessingController {
     private $json = file_get_contents('connections.json');
     private $obj = json_decode($this->json, TRUE);  
+    private $randomValues = array();
     
     function getRandomCats(){
-        global $obj;
         $randomCats = array();
-        $randomValues = array();
         for($i = 1; $i <= 4; $i++){
-            $key = array_rand($obj);
-            $list = $obj[$key];
+            $key = array_rand($this->obj);
+            $list = $this->obj[$key];
             shuffle($list);
             if(!array_key_exists($key, $randomValues)){
                 for($j=0; $j < 4; $j++){
