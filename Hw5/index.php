230e9a26e9be9e4fd1ee5e9e7447c67207c23da3
akl5sz <akl5sz@virginia.edu>
2024-03-31 21:48:29 -0400

    added link and commented out error handling

--- Hw5/index.php
+++ Hw5/index.php
@@ -1,5 +1,8 @@
 <?php
-// Sources used: https://cs4640.cs.virginia.edu,
+// LINK: https://cs4640.cs.virginia.edu/akl5sz/hw5/?command=logout
+
+// Sources used: 
+//https://cs4640.cs.virginia.edu,
 //https://stackoverflow.com/questions/10258345/php-simple-foreach-loop-with-html
 //https://stackoverflow.com/questions/369602/deleting-an-element-from-an-array-in-php
 //https://stackoverflow.com/questions/37404583/php-guessing-number
@@ -9,8 +12,8 @@
 //link to published site for Angie Loayza (akl5sz): https://cs4640.cs.virginia.edu/akl5sz/hw5/
 //
 // DEBUGGING ONLY! Show all errors.
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
 
 // Class autoloading by name.  All our classes will be in a directory
 // that Apache does not serve publicly.  They will be in /opt/src/, which
