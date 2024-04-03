6abfe7d3072a1db503ae071fe27d1833678181d5
akl5sz <akl5sz@virginia.edu>
2024-04-03 00:35:16 -0400

    added some code and files

--- Sprint3/feed.php
+++ Sprint3/feed.php
@@ -49,7 +49,7 @@
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                <button class="btn btn-primary" type="submit">Button</button>
+                <button class="btn btn-primary" type="submit"><i>Show Anyways</i></button>
               </div>
             </div>
           </div>
@@ -67,7 +67,7 @@
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                <button class="btn btn-primary" type="submit">Button</button>
+                <button class="btn btn-primary" type="submit"><i>Show Anyways</i></button>
               </div>
             </div>
           </div>
