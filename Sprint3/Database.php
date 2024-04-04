a877b61e1df422b18f37b4a82695cdae136890f4
jelainhart <nyt8te@virginia.edu>
2024-04-04 17:57:26 -0400

    fixed schemas

--- Sprint3/Database.php
+++ Sprint3/Database.php
@@ -45,30 +45,33 @@ class Database {
                 'nyt8te','Jackie', 'nyt8te@virginia.edu',
                 password_hash('urmuth4', PASSWORD_DEFAULT));
         
-        $res  = pg_query($this->dbHandle, "create table if not exists movies (
-                title text,
-                year int,
-                rating text,
-                director text,
-                hours int,
-                minutes int,
-                description text,
-                primary key (title, year));");
-        
-        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
-                'The Muppet Movie', 1979, 'G', 'James Frawley', 1, 37, 'After Kermit the Frog decides to pursue a movie career, he starts his cross-country trip from Florida to California. Along the way, he meets and befriends Fozzie Bear, Miss Piggy, Gonzo and rock musicians Dr. Teeth and the Electric Mayhem. When Kermit is offered a job by Doc Hopper (Charles Durning) to advertise the fried frog legs at his restaurant chain, Kermit turns Hopper down. However, Hopper refuses to relent and pursues Kermit and his companions to a final showdown.');         
+                $res  = pg_query($this->dbHandle, "create table if not exists movies (
+                    title text,
+                    year int,
+                    rating text,
+                    director text,
+                    hours int,
+                    minutes int,
+                    description text,
+                    primary key (title, year));");
+            
+            $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) 
+                                 values ($1, $2, $3, $4, $5, $6, $7)
+                                 on conflict do nothing;",
+                    'The Muppet Movie', 1979, 'G', 'James Frawley', 1, 37, 
+                    'After Kermit the Frog decides to pursue a movie career, he starts his cross-country trip from Florida to California. Along the way, he meets and befriends Fozzie Bear, Miss Piggy, Gonzo and rock musicians Dr. Teeth and the Electric Mayhem. When Kermit is offered a job by Doc Hopper (Charles Durning) to advertise the fried frog legs at his restaurant chain, Kermit turns Hopper down. However, Hopper refuses to relent and pursues Kermit and his companions to a final showdown.');          
                 
         $res  = pg_query($this->dbHandle, "create table if not exists genres (
                 genre text,
                 title text,
                 year int,
-                foreign key (title, year) references movies (title, year))
-                primary key (title, year));");
+                primary key (genre, title, year),
+                foreign key (title, year) references movies (title, year));");
 
-        $res = $this->query("insert into genres (movie_title, movie_year, genre) values ($1, $2, $3);",
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
                 'The Muppet Movie', 1979, 'Musical');
 
-        $res = $this->query("insert into genres (movie_title, movie_year, genre) values ($1, $2, $3);",
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
                 'The Muppet Movie', 1979, 'Adventure');      
 
 
