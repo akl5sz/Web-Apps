602ccd12a9f9ce36534ff24f7e10255c1364b0de
akl5sz <akl5sz@virginia.edu>
2024-04-04 18:23:41 -0400

    added more movies and genres

--- Sprint3/Database.php
+++ Sprint3/Database.php
@@ -45,47 +45,97 @@ class Database {
                 'nyt8te','Jackie', 'nyt8te@virginia.edu',
                 password_hash('urmuth4', PASSWORD_DEFAULT));
         
-                $res  = pg_query($this->dbHandle, "create table if not exists movies (
-                    title text,
-                    year int,
-                    rating text,
-                    director text,
-                    hours int,
-                    minutes int,
-                    description text,
-                    primary key (title, year));");
+        $res  = pg_query($this->dbHandle, "create table if not exists movies (
+                title text,
+                year int,
+                rating text,
+                director text,
+                hours int,
+                minutes int,
+                description text,
+                primary key (title, year));");
             
-            $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) 
-                                 values ($1, $2, $3, $4, $5, $6, $7);",
-                    'The Muppet Movie', 1979, 'G', 'James Frawley', 1, 37, 
-                    'After Kermit the Frog decides to pursue a movie career, he starts his cross-country trip from Florida to California. Along the way, he meets and befriends Fozzie Bear, Miss Piggy, Gonzo and rock musicians Dr. Teeth and the Electric Mayhem. When Kermit is offered a job by Doc Hopper (Charles Durning) to advertise the fried frog legs at his restaurant chain, Kermit turns Hopper down. However, Hopper refuses to relent and pursues Kermit and his companions to a final showdown.');          
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7);",
+                'The Muppet Movie', 1979, 'G', 'James Frawley', 1, 37, 
+                'After Kermit the Frog decides to pursue a movie career, he starts his cross-country trip from Florida to California. Along the way, he meets and befriends Fozzie Bear, Miss Piggy, Gonzo and rock musicians Dr. Teeth and the Electric Mayhem. When Kermit is offered a job by Doc Hopper (Charles Durning) to advertise the fried frog legs at his restaurant chain, Kermit turns Hopper down. However, Hopper refuses to relent and pursues Kermit and his companions to a final showdown.');          
             
                     
-            $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) 
-                            values ($1, $2, $3, $4, $5, $6, $7);",
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7);",
                 'The Hunger Games', 2012, 'PG-13', 'Gary Ross', 2, 26, 
                 'In what was once North America, the Capitol of Panem maintains its hold on its 12 districts by forcing them each to select a boy and a girl, called Tributes, to compete in a nationally televised event called the Hunger Games. Every citizen must watch as the youths fight to the death until only one remains. District 12 Tribute Katniss Everdeen (Jennifer Lawrence) has little to rely on, other than her hunting skills and sharp instincts, in an arena where she must weigh survival against love.');
                 
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
+                'Coraline', 2009, 'PG', 'Henry Selick', 1, 40, 'Wandering her rambling old house in her boring new town, an 11-year-old Coraline discovers a hidden door to a strangely idealized version of her life. In order to stay in the fantasy, she must make a frighteningly real sacrifice.');
+                //Animation, Drama, Family
+
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
+                'Bee Movie', 2007, 'PG', 'Simon J. Smith', 1, 31, 'Barry B. Benson, a bee just graduated from college, is disillusioned at his lone career choice: making honey. On a special trip outside the hive, Barry\'s life is saved by Vanessa, a florist in New York City. As their relationship blossoms, he discovers humans actually eat honey and subsequently decides to sue them.');
+                //Animation, Adventure, Comedy
+
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
+                'Chicken Run', 2000, 'G', 'Nick Park', 1, 24, 'When a cockerel apparently flies into a chicken farm, the chickens see him as an opportunity to escape their evil owners.');
+                //Animation, Adventure, Comedy
+
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
+                'Shrek', 2001, 'PG', 'Andrew Adamson', 1, 30, 'A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.');
+                //Animation, Adventure, Comedy
+
+        $res = $this->query("insert into movies (title, year, rating, director, hours, minutes, description) values ($1, $2, $3, $4, $5, $6, $7)",
+                'Fifty Shades of Grey', 2015, 'R', 'Sam Taylor-Johnson', 2, 5, 'Literature student Anastasia Steele\'s life changes forever when she meets handsome, yet tormented, billionaire Christian Grey.');              
+                //Drama, Romance, Thriller
+
                 
-            $res  = pg_query($this->dbHandle, "create table if not exists genres (
-                    genre text,
-                    title text,
-                    year int,
-                    primary key (genre, title, year),
-                    foreign key (title, year) references movies (title, year));");
+        $res  = pg_query($this->dbHandle, "create table if not exists genres (
+                genre text,
+                title text,
+                year int,
+                primary key (genre, title, year),
+                foreign key (title, year) references movies (title, year));");
 
-            $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
-                    'The Muppet Movie', 1979, 'Musical');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'The Muppet Movie', 1979, 'Musical');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'The Muppet Movie', 1979, 'Adventure');
 
-            $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
-                    'The Muppet Movie', 1979, 'Adventure');
-            $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
-                    'The Hunger Games', 2012, 'Action');
-            $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
-                    'The Hunger Games', 2012, 'Sci-Fi');               
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'The Hunger Games', 2012, 'Action');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'The Hunger Games', 2012, 'Sci-Fi');  
+
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Coraline', 2009, 'Animation');      
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Coraline', 2009, 'Drama');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Coraline', 2009, 'Family');
+
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Bee Movie', 2007, 'Animation');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Bee Movie', 2007, 'Adventure');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Bee Movie', 2007, 'Comedy');
+
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Chicken Run', 2000, 'Animation');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Chicken Run', 2000, 'Adventure');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Chicken Run', 2000, 'Comedy');
               
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Shrek', 2001, 'Animation');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Shrek', 2001, 'Adventure');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Shrek', 2001, 'Comedy');
 
-
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Fifty Shades of Grey', 2015, 'Drama');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Fifty Shades of Grey', 2015, 'Romance');
+        $res = $this->query("insert into genres (title, year, genre) values ($1, $2, $3);",
+                'Fifty Shades of Grey', 2015, 'Thriller');
     }
     
 
